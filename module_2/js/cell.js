2e5c25c47e24eb24627c87bc1a0b2e4acd426ef9
RamazaN <ramazan.abdullaev.00@gmail.com>
2022-03-14 18:14:06 +0300

    add dinamics to the game

--- module_2/js/cell.js
+++ module_2/js/cell.js
@@ -2,13 +2,13 @@ class Cell{
 	constructor(fieldElement){
 		this.element = document.createElement('div');
 		this.element.className = 'cell';
-		if(Math.random() > 0.8){
-			this.value  = Math.random() > 0.5? 4 : 2;
+		if(Math.random() > 0.7){
+			this.spawn();
 		}
 		fieldElement.appendChild(this.element); 
-		this.element.onclick = function(e){
-			this.merge();
-		}.bind(this);
+		//this.element.onclick = function(e){
+		//	this.merge();
+		//}.bind(this);
 	}
 	get value(){
 		return this._value || 0;
@@ -20,8 +20,19 @@ class Cell{
 	clear(){
 		this.value = '';
 	}
-	merge(){
-		this.value *= 2;
+	merge(cell){
+		if(this.value < 2048){
+			this.value += cell.value;
+			cell.clear();
+		}
 	}
-
+	isSameTo(cell){
+		return this.value == cell.value;
+	}
+	spawn(){
+		this.value =  Math.random() > 0.8? 4 : 2;
+	}
+	get isEmpty(){
+		return this.value == 0;
+	}
 }
--- module_2/js/game.js
+++ module_2/js/game.js
@@ -1,5 +1,7 @@
+
 class Game{
 	constructor(parentElement,size=4){
+		this.size = size;
 		let gameFieldElement = document.createElement('div');
 		gameFieldElement.className = 'game';
 		parentElement.appendChild(gameFieldElement);
@@ -17,12 +19,209 @@ class Game{
 
 		let fieldElement = document.createElement('div');
 		fieldElement.className = 'field';
-
 		gameFieldElement.appendChild(fieldElement);
+
+		this.field = [];
+
 		for(let i=0; i < size; i++) {
+			this.field[i] = [];
 			for(let j=0; j < size; j++){
-				new Cell(fieldElement);
+				this.field[i][j] = new Cell(fieldElement);
 			}
+		}
+
+		window.onkeyup = function(e) {
+			switch(e.keyCode){
+				case 38:
+					this.moveUp();
+					break;
+				case 40:
+					this.moveDown();
+					break;
+				case 37:
+					this.moveLeft();
+					break;
+				case 39:
+					this.moveRight();
+					break;
+			}
+		}.bind(this);
+
+		console.log(this.field);
+	}
+
+	spawnUnit(){
+		let emptyCells = [];
+		for(let i = 0; i < this.field.length; i++){
+			for(let j = 0; j < this.field[i].length; j++){
+				if(!this.field[i][j].value){
+					emptyCells.push(this.field[i][j]);
+				}
+			}
+		}
+		if(emptyCells.length){
+			emptyCells[getRandomInt(0, emptyCells.length - 1)].spawn();
+		}else{
+			alert('You lose!');
+		}
+	
+	}
+
+
+	isLastKey(key){
+		return key == (this.size - 1);
+	}
+
+	isFirstKey(key){
+		return key == 0;
+	}
+
+
+
+	moveLeft(){
+		let hasMoved = false;
+		for(let i = 0; i < this.size; i++){
+			for(let j = 1; j < this.size; j++){
+				let currentCell = this.field[i][j];
+				if(currentCell.isEmpty){
+					continue;
+				}
+				let nextCellKey = j - 1;
+				while(nextCellKey >= 0){
+
+					let nextCell = this.field[i][nextCellKey];
+
+					if(!nextCell.isEmpty || this.isFirstKey(nextCellKey)){
+						if((nextCell.isEmpty && this.isFirstKey(nextCellKey))
+						|| nextCell.isSameTo(currentCell)){
+							this.field[i][nextCellKey].merge(currentCell);
+							hasMoved = true;
+						} else if((!nextCell.isEmpty && nextCellKey + 1 != j)){
+							this.field[i][nextCellKey + 1].merge(currentCell);
+							hasMoved = true;
+						}
+						break;
+					}
+					nextCellKey--;
+					nextCell = this.field[i][nextCellKey];
+				}
+			}
+		}
+
+		if(hasMoved){
+			this.spawnUnit();
+		}
+	}
+
+
+
+	moveRight(){
+		let hasMoved = false;
+		for(let i = 0; i < this.field.length; i++){
+			for(let j = this.field[i].length - 2; j >= 0; j--){
+				let currentCell = this.field[i][j];
+				if(currentCell.isEmpty){
+					continue;
+				}
+				let nextCellKey = j + 1;
+				while(nextCellKey < this.size){
+
+					let nextCell = this.field[i][nextCellKey];
+
+					if(!nextCell.isEmpty || this.isLastKey(nextCellKey)){
+						if((nextCell.isEmpty && this.isLastKey(nextCellKey))
+						|| nextCell.isSameTo(currentCell)){
+							this.field[i][nextCellKey].merge(currentCell);
+							hasMoved = true;
+						} else if((!nextCell.isEmpty && nextCellKey - 1 != j)){
+							this.field[i][nextCellKey - 1].merge(currentCell);
+							hasMoved = true;
+						}
+						break;
+					}
+					nextCellKey++;
+					nextCell = this.field[i][nextCellKey];
+				}
+			}
+		}
+
+		if(hasMoved){
+			this.spawnUnit();
+		}
+	}
+
+
+	moveDown(){
+		let hasMoved = false;
+		for(let j = 0; j < this.size; j++){
+			for(let i = this.size - 2; i >= 0; i--){
+				let currentCell = this.field[i][j];
+				if(currentCell.isEmpty){
+					continue;
+				}
+				let nextCellKey = i + 1;
+				while(nextCellKey < this.size){
+
+					let nextCell = this.field[nextCellKey][j];
+
+					if(!nextCell.isEmpty || this.isLastKey(nextCellKey)){
+						if((nextCell.isEmpty && this.isLastKey(nextCellKey))
+						|| nextCell.isSameTo(currentCell)){
+							this.field[nextCellKey][j].merge(currentCell);
+							hasMoved = true;
+						} else if((!nextCell.isEmpty && nextCellKey - 1 != i)){
+							this.field[nextCellKey - 1][j].merge(currentCell);
+							hasMoved = true;
+						}
+						break;
+					}
+					nextCellKey++;
+					nextCell = this.field[nextCellKey][j];
+				}
+			}
+		}
+
+		if(hasMoved){
+			this.spawnUnit();
+		}
+	}
+	
+
+
+
+	moveUp(){
+		let hasMoved = false;
+		for(let j = 0; j < this.size; j++){
+			for(let i = 1; i < this.size; i++){
+				let currentCell = this.field[i][j];
+				if(currentCell.isEmpty){
+					continue;
+				}
+				let nextCellKey = i - 1;
+				while(nextCellKey < this.size){
+
+					let nextCell = this.field[nextCellKey][j];
+
+					if(!nextCell.isEmpty || this.isFirstKey(nextCellKey)){
+						if((nextCell.isEmpty && this.isFirstKey(nextCellKey))
+						|| nextCell.isSameTo(currentCell)){
+							this.field[nextCellKey][j].merge(currentCell);
+							hasMoved = true;
+						} else if((!nextCell.isEmpty && nextCellKey + 1 != i)){
+							this.field[nextCellKey + 1][j].merge(currentCell);
+							hasMoved = true;
+						}
+						break;
+					}
+					nextCellKey--;
+					nextCell = this.field[nextCellKey][j];
+				}
+			}
+		}
+
+		if(hasMoved){
+			this.spawnUnit();
 		}
 	}
 }
+
